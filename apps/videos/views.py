05606b95cdd9f03b97da448c8578a940818a600d
Michael Nelli <mrn73@scarletmail.rutgers.edu>
2023-04-10 14:04:20 -0400

    Changed queryset to only filter on 'list' action.
    This is for more predictable http response codes. Filtering the
    queryset for all actions led to 403s and 404s, depending on if the user
    could see the resource at all. If a user could see a resource but not
    edit it, it would return 403; otherwise, if the user couldn't see the
    video and also couldn't edit it, it would return 404. Now we just use
    Video.objects.all() which ensures a 404 if the resource doesn't exist, or
    403 if it does but it can't be accessed.

--- apps/videos/views.py
+++ apps/videos/views.py
@@ -4,25 +4,35 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions
 from apps.videos.models import Video
 from apps.videos.serializers import VideoSerializer
+from apps.videos.permissions import IsCreator, IsShared
 
 class VideoViewSet(ModelViewSet):
+    queryset = Video.objects.all()
     serializer_class = VideoSerializer
+    # Disables PUT
+    http_method_names = ['get', 'post', 'patch', 'delete', 'head', 'options']
 
     def get_permissions(self):
-        permission_classes = [permissions.AllowAny]
-        if self.action not in ['list', 'retrieve']:
-            permission_classes = [permissions.IsAuthenticated]
+        if self.action == 'list':
+            permission_classes = [permissions.AllowAny]
+        elif self.action == 'retrieve':
+            permission_classes = [((permissions.IsAuthenticated & IsCreator) | IsShared) | permissions.IsAdminUser]
+        else:
+            permission_classes = [permissions.IsAuthenticated & (IsCreator | permissions.IsAdminUser)]
         return [permission() for permission in permission_classes]
 
     def get_queryset(self):
+        queryset = super().get_queryset()
         user = self.request.user
-        # include all public videos by default
-        queryset = Video.objects.filter(is_public=True)
-        if user.is_authenticated:
-            # admins can see all videos
-            if user.is_staff:
-                return Video.objects.all()
-            queryset |= Video.objects.filter(creator=user)
+        
+        # Filter the listed videos to videos the user can see if the user is not a staff member
+        if self.action == 'list' and not (user.is_authenticated and user.is_staff):
+            # Include all public videos by default
+            queryset = Video.objects.filter(is_public=True)
+            if user.is_authenticated:
+                #TODO: also add videos that are shared with the user
+                queryset |= Video.objects.filter(creator=user)
+
         return queryset
     
     def perform_destroy(self, instance):
