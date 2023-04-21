6b27971ae85f64c5e7a5c891295b6af6a618bf20
Michael Nelli <mrn73@scarletmail.rutgers.edu>
2023-04-21 13:40:11 -0400

    Added url routes to new friendship views.

--- apps/friendships/urls.py
+++ apps/friendships/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.friendships.views import ListCreateFriendshipView, ListPendingFriendshipView, FriendshipDetailView
+from apps.friendships.views import ListCreateFriendshipView, FriendshipDetailView, IncomingFriendRequestView, OutgoingFriendRequestView
 
 urlpatterns = [
         path('users/<int:user_id>/friends', ListCreateFriendshipView.as_view(), name='user-friends'),
-        path('users/<int:user_id>/friends/pending', ListPendingFriendshipView.as_view(), name='user-friends-pending'),
+        path('users/<int:user_id>/friends/incoming-requests', IncomingFriendRequestView.as_view(), name='user-friends-incoming'),
+        path('users/<int:user_id>/friends/outgoing-requests', OutgoingFriendRequestView.as_view(), name='user-friends-outgoing'),
         path('friendships/<int:pk>', FriendshipDetailView.as_view(), name='friendship-detail') 
 ]
