17f5990ff3b129bb0ff87302c3f643577b2e4475
Michael Nelli <mrn73@scarletmail.rutgers.edu>
2023-05-02 01:49:14 -0400

    Added a NestedUserDefault serializer default field
    Designed to eventually replace CurrentUserDefault in serializer classes.
    This will solve the issue of admins not being able to update a user's
    resource for them, as the "creator" or "owner" fields can be
    accidentally set to the admin since "CurrentUserDefault" evaluates to them.

--- utils/defaults.py
+++ utils/defaults.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 
 class CurrentUserIDDefault:
     '''
@@ -18,6 +19,41 @@ class CurrentUserIDDefault:
 
     def __call__(self, serializer_field):
         return serializer_field.context['request'].user.id
+
+    def __repr__(self):
+        return '%s()' % self.__class__.__name__
+
+class NestedUserDefault:
+    '''
+    Returns the parent user in the URI. ONLY FOR 1 NESTED LEVEL!
+
+    This is useful when you want resources under a user, but want admins to
+    be able to create/edit resources for that user. CurrentUserDefault uses
+    the logged-in user, so having something like a creator field on the
+    resource would default to the admin rather than the user upon writing.
+    
+    NOTE: This only works is user is the only parent!
+        users/<pk>/groups --> we would use pk to get the User
+        
+        If we had instead:
+        users/<pk>/groups/<pk> --> this class may not work correctly
+    '''
+    requires_context = True
+
+    def __call__(self, serializer_field):
+        kwargs = serializer_field.context.get('view', None)
+        if kwargs is None:
+            raise ValueError("Must contain the view in the context dict")
+        # Get first element in the kwargs dict. None if dict is empty.
+        user_id = next(iter(kwargs.values()), None)
+        if user_id is None:
+            raise ValueError("There is no parent user resource in the URI")
+        User = get_user_model()
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            user = None
+        return user
 
     def __repr__(self):
         return '%s()' % self.__class__.__name__
